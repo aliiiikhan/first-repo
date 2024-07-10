4244af3e8de8cd3d2756229cc1f9d361543f8709
aliiiiiikhan <kali67030@gmail.com>
2024-07-10 22:38:37 +0500

    Commit

--- Quiz 1/q2.dart
+++ Quiz 1/q2.dart
@@ -9,11 +9,15 @@ Output: 153 is an Armstrong number
 import 'dart:math';
 
 void main() {
+
+  
   for (int i = 1; i < 1000; i++) {
-    num original_number_given = i;
+    num original_number_given;
+    original_number_given = i;
     num sum = 0;
     while (original_number_given > 0) {
-      num digit_for_add = original_number_given % 10;
+      num digit_for_add;
+      digit_for_add = original_number_given % 10;
       sum += pow(digit_for_add, 3);
       original_number_given = (original_number_given / 10).toInt();
     }
